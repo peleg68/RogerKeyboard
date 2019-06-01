18cf1f45b78a0244d2eebc87405dcbfab89479eb
Unknown <pelegtsa@gmail.com>
2019-06-01 16:48:12 +0300

    Fixed test

--- app/src/androidTest/java/org/pelegtsadok/rogerkeyboard/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/pelegtsadok/rogerkeyboard/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.emexezidis.hashtagkeyboard", appContext.getPackageName());
+        assertEquals("org.pelegtsadok.rogerkeyboard", appContext.getPackageName());
     }
 }
